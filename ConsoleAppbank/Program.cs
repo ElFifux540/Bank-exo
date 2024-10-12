54935f364dc5f9349939262b8010a6a8c50ee739
ElFifux540 <125292427+ElFifux540@users.noreply.github.com>
2024-10-12 12:58:15 +0200

    Step 1 and Step2 done

--- ConsoleAppbank/Program.cs
+++ ConsoleAppbank/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public Person(string firstName, string lastName, DateTime birthDate)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+    }
+}
+
+public class CurrentAccount
+{
+    public string Number { get; set; }
+    public double Balance { get; private set; }
+    public double CreditLine { get; set; }
+    public Person Owner { get; set; }
+
+    public CurrentAccount(string number, double initialBalance, double creditLine, Person owner)
+    {
+        Number = number;
+        Balance = initialBalance;
+        CreditLine = creditLine;
+        Owner = owner;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+
+        if (Balance - amount < -CreditLine)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+
+        Balance += amount;
+    }
+
+    public double GetBalance()
+    {
+        return Balance;
+    }
+}
